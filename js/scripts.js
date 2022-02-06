337bca6caea652ffe2aa741f31590b543c5147f1
Alex BO <alexandreb09@yahoo.fr>
2022-02-06 12:00:40 +0100

    :fire: Add medecine category + Wheel :fire:

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
-//
-// Scripts
-// 
+import {Wheel} from './spin-wheel-esm.min.js';
 
-window.addEventListener('DOMContentLoaded', () => {
+const prop = {
+    name: 'Workout',
+    radius: 0.84,
+    itemLabelRadius: 0.93,
+    itemLabelRadiusMax: 0.35,
+    itemLabelRotation: 180,
+    itemLabelAlign: 'left',
+    itemLabelColors: ['#fff'],
+    itemLabelBaselineOffset: -0.07,
+    lineColor: '#fff',
+    itemBackgroundColors: ['#ffc93c', '#66bfbf', '#a2d5f2', '#515070', '#43658b', '#ed6663', '#d54062' ],
+    image: './assets/img/example-0-image.svg',
+    overlayImage: './assets/img/example-0-overlay.svg',
+    rotationSpeedMax: 750,
+}
+
+
+$(document).ready( () => {
+    
     // Navbar shrink function
-    var navbarShrink = function () {
+    let navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+        if (!navbarCollapsible) return;
+        if (window.scrollY === 0) navbarCollapsible.classList.remove('navbar-shrink');
+        else navbarCollapsible.classList.add('navbar-shrink');
     };
-
+    
     // Shrink the navbar 
     navbarShrink();
-
+    
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
-
-    // // Activate Bootstrap scrollspy on the main nav element
-    // const mainNav = document.body.querySelector('#mainNav');
-    // if (mainNav) {
-    //     new bootstrap.ScrollSpy(document.body, {
-    //         target: '#mainNav',
-    //         offset: 72,
-    //     });
-    // };
-
+    
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
+    const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
@@ -43,75 +43,109 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-
+        
     /*******************************************/
     /*                  ITEMS                  */
     /*******************************************/
-    const items_default = ["Pates", "Blé", "Riz", "Semoule"].sort(() => 0.5 - Math.random());;
-    let isSubmitting = false;
+    const $itmLst = $("#liste-items");
+    const $itmAdd = $("#item-add");
+    const $mdlDom = $("#modal");
+    const $mdlTxt = $("#modal #txt-modal-result");
+    const $mdlCls = $("#modal #modal-close");
+    const $btnWhl = $("#turn-wheel-btn");
+    const $chgTpe = $("#list-type a");
+    
+    // Create wheel Object 
+    const wheel = new Wheel(document.querySelector('.wheel-wrapper'));
+    
+    let data = {
+        itmLst : $itmLst,
+        itmAdd : $itmAdd,
+        mdlDom : $mdlDom,
+        mdlTxt : $mdlTxt,
+        mdlCls : $mdlCls,
+        wheel  : wheel,
+    };
+    
+    switchType(type_displayed, data);
 
-    let addItem = function(itemStr){
-        // Create Dom
-        let itemDom = $('<div class="btn btn-item btn-outline-light mx-1 my-2" data-item="' + itemStr + '"></div>');
-        let removeIcon = $('<span class="ms-3" title="Cliquez ici pour supprimer une valeur"><i class="fa fa-times" aria-hidden="true"></i></span>');
-        $("#liste-items").append(itemDom.append(itemStr, removeIcon));
-        // On Item remove
-        removeIcon.on("click", null, null, () => itemDom.fadeOut(300, function() { $(this).remove(); }));
+    init_wheel(data);
+
+    $itmAdd.on("keydown", null, data, onitmAdd);
+    $mdlCls.on('click', null, null, () => $mdlDom.modal('hide'));
+    $btnWhl.on("click", null, null, () => data.wheel.spin(data.wheel.rotationSpeed + Math.random() * 50 + 230));
+
+    $chgTpe.each((i,e) => $(e).on("click", null, null, () => switchType($(e).data("type"), data)));
+});
+
+function init_wheel(data) {
+    const items = readItems(data.itmLst);
+    data.wheel.init({
+        ...prop,
+        items: items,
+        rotationSpeed: data.rotationSpeed ? data.rotationSpeed : data.wheel.rotationSpeed,
+        rotation: data.wheel.rotation,    // Preserve value so it doesn't get reset.
+        onRest: e => {
+            data.mdlTxt.html(translations[type_displayed]["txt-modal-result"].replace("%value%", items[e.currentIndex].label));
+            data.mdlDom.modal('show');
+        },
+    });
+}   
+
+function addItem(itemStr, data){
+    // Create Dom for the new item
+    let $itemDom = $('<div data-role="item" class="btn btn-item btn-outline-light mx-1 my-2" data-item="' + itemStr + '"></div>');
+    let $removeIcon = $('<span class="ms-3" title="Cliquez ici pour supprimer l\'élément"><i class="fa fa-times" aria-hidden="true"></i></span>');
+    data.itmLst.append($itemDom.append(itemStr, $removeIcon));
+    // Event listener
+    $removeIcon.on("click", null, data, onItemRemove);
+}
+
+function onitmAdd(e){
+    const $itmAdd = e.data.itmAdd;
+    const $itmLst = e.data.itmLst;
+
+    if(e.key === 'Enter') {
+        let new_item_str = $itmAdd.val();
+        // Get all the items in cards
+        let items_all = [];
+        $itmLst.children().each((i,e) => items_all.push($(e).data("item").toLowerCase()));
+        // Check if item already exist
+        if (items_all.includes(new_item_str.toLowerCase())){
+            alert("L'élément \"" + new_item_str + "\" est déjà présent dans la liste.");
+        } else {
+            addItem(new_item_str, e.data);
+            init_wheel(e.data);
+        } 
+        $itmAdd.val("");
+    }
+}
+
+function onItemRemove(e){
+    $(this).closest("[data-role=\"item\"]").fadeOut(300, function() { 
+        $(this).remove(); 
+        init_wheel(e.data);
+    });
+}
+
+function readItems($itmLst){
+    return $itmLst.children().map(function(){return {"label": $(this).data('item')};}).get();
+}
+
+
+function switchType(type, data){
+    type_displayed = type;
+
+    // Update translations messages
+    for (const [domId, message] of Object.entries(translations[type])) {
+        $("#" + domId).html(message);
     }
 
-    let randomIntFromInterval = function(min, max) { return Math.floor(Math.random() * (max - min + 1) + min)}
+    data.itmLst.empty();
 
-    items_default.forEach(addItem);
-
-    $("#item-add").on("keydown", null, null, (e) => {
-        if(e.key === 'Enter') {
-            let new_item_str = $("#item-add").val();
-            // Get all the items in cards
-            let items_all = [];
-            $("#liste-items").children().each((i,e) => items_all.push($(e).data("item").toLowerCase()));
-            // Check if item already exist
-            if (items_all.includes(new_item_str.toLowerCase())){
-                alert("L'élément \"" + new_item_str + "\" est déjà présent dans la liste.")
-            } else {
-                addItem(new_item_str);
-            } 
-            $("#item-add").val("");
-        }
-    });
-
-    $("#submitButton").on("submit click", function(){
-        // Prevent multiple submission
-        if (isSubmitting) return;
-        isSubmitting = true;
-
-        let childrenDom = $("#liste-items").children();
-        let rand_position = randomIntFromInterval(0, childrenDom.length);
-
-        childrenDom.each((i,e) => {$(e).css({background: "transparent"});})
-
-        let computeDelay = x => 100 * Math.sin(((Math.min(x, 75)-55)/(22))-0.4)+150;
-        
-        let highlightDom = function(i, n, childrenDom){
-            let $item = childrenDom.eq(i % childrenDom.length);
-            if (i === n){
-                $item.css({background: "rgb(26, 87, 132)"});
-                isSubmitting = false;
-                return;
-            };
-
-            $item.css({background: "rgb(26, 87, 132)"})
-                .delay(computeDelay(i))
-                .promise()
-                .done(function(){
-                    $item.css({background: "transparent"})
-                        .delay(20)
-                        .promise()
-                        .done(function () {
-                            highlightDom(i + 1, n, childrenDom);
-                        });
-                })
-            ;
-        }
-        highlightDom(0, 80 + rand_position, childrenDom);
-    });
-});
+    items_default[type].map(e => ({'label': e}))
+        .sort(() => 0.5 - Math.random())
+        .forEach(item => addItem(item.label, data));
+    
+    init_wheel(data);
+}
